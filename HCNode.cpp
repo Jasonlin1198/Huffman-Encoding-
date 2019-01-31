3678d2cfeb217a42c0a732b4880edc3d3c6a318a
cjalvarado <cjalvarado@eng.ucsd.edu>
2019-01-31 13:47:30 -0800

    still moving files around

--- HCNode.cpp
+++
@@ -1,9 +0,0 @@
-#include "HCNode.hpp"
-
-/** Less-than comparison, so HCNodes will work in std::priority_queue
- *  We want small counts to have high priority.
- *  And we want to break ties deterministically.
- */
-bool HCNode::operator<(const HCNode& other) {
-    return false;  // TODO (checkpoint)
-}
